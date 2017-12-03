48a03b45605017604cd62d2f2b94691ecb51afce
Ryo UMETSU <uryooo@gmail.com>
2017-12-03 23:05:18 +0900

    Introduced Timber(Timberkt) and IcePick(Pikkel) and checking 404 error

--- app/src/main/kotlin/bz/mydns/walt/u_ryo/proplister/MainActivity.kt
+++ app/src/main/kotlin/bz/mydns/walt/u_ryo/proplister/MainActivity.kt
@@ -5,43 +5,49 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import android.text.TextUtils
-import android.util.Log
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
+import com.github.ajalt.timberkt.Timber
+import com.github.ajalt.timberkt.d
+import com.github.ajalt.timberkt.e
+import com.github.yamamotoj.pikkel.Pikkel
+import com.github.yamamotoj.pikkel.PikkelDelegate
 import io.reactivex.Single
 import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_main.*
+import retrofit2.HttpException
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 
-class MainActivity: AppCompatActivity() {
+class MainActivity: AppCompatActivity(), Pikkel by PikkelDelegate() {
     private val url = "https://api.github.com/"
-    private var adapter: RecyclerViewAdapter? = null
-    private val listKey = "list"
-    private val tag = "MainActivity"
+    private var adapter by state<RecyclerViewAdapter?>(null)
+    private lateinit var single: Disposable
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Timber.plant(timber.log.Timber.DebugTree())
         recyclerView.layoutManager = LinearLayoutManager(this)
-        adapter = savedInstanceState?.getParcelable(listKey)
-        if (adapter != null) {
-            Log.d(tag, adapter?.toString())
+        restoreInstanceState(savedInstanceState)
+        adapter?.run {
+            d { "adapter: $adapter" }
             recyclerView.adapter = adapter
         }
     }
 
     override fun onSaveInstanceState(outState: Bundle?) {
-        outState?.putParcelable(listKey, adapter)
         super.onSaveInstanceState(outState)
+        saveInstanceState(outState)
     }
 
     fun submit(button: View) {
         if (!TextUtils.isEmpty(textField.text)) {
-            Single.using(
+            single = Single.using(
                     { setProcessBar(true) },
                     { setRetrofit(url) },
                     { setProcessBar(false) })
@@ -73,7 +79,7 @@ class MainActivity: AppCompatActivity() {
     }
 
     private fun processResults(results: List<Repository>) {
-        Log.d(tag, results.toString())
+        d { "results: $results" }
         adapter = RecyclerViewAdapter(results.map { r -> r.name })
         recyclerView.adapter = adapter
         (getSystemService(Context.INPUT_METHOD_SERVICE)
@@ -84,8 +90,9 @@ class MainActivity: AppCompatActivity() {
     }
 
     private fun processError(error: Throwable) {
-        Log.e(tag, "ERROR!", error)
-        showToast(R.string.network_error)
+        e { "ERROR: ${error.message}" }
+        showToast(if (error is HttpException && error.code() == 404)
+            R.string.no_such_user_error else R.string.network_error)
     }
 
     private fun showToast(message: Int) {
